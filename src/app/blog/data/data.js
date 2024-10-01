f7249f787ec6313296b53e561e991e5aa1191329
Igor <igor.oliveira.contact2210@gmail.com>
2024-10-01 15:11:38 -0300

    adicionadno o arquivo de dados dos artigos)

---
+++ src/app/blog/data/data.js
@@ -0,0 +1,12 @@
+export const articles = [
+  {
+    title: "O que é a inteligência artificial?",
+    description:
+      "Esse artigo explica o que é uma inteligência artificial e mostra as mudanças que a inteligência artificial trouxe para a internet e para as pessoas.",
+    date: "30 de setembro",
+    readTime: 15,
+    path: "/blog/artigos/o-que-e-inteligencia-artificial",
+    category: "noticias-e-tendencias",
+    main: true,
+  },
+];
